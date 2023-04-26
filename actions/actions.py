12446ce9399d946ef0e921e738204e95836bc715
Theamineguesmi <49189239+Theamineguesmi@users.noreply.github.com>
2023-04-26 16:27:34 +0100

    Update actions.py
    OpenAI api key update

--- actions/actions.py
+++ actions/actions.py
@@ -5,7 +5,7 @@ import datetime
 import openai
 from rasa_sdk.events import SlotSet
 
-openai.api_key = ("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")
+openai.api_key = ("sk-")
 
 from pathlib import Path
 from typing import Any, Text, Dict, List
